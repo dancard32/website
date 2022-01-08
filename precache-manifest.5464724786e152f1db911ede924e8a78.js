cbbea00440988c03c4133e3679dc371588ee9d5f
Dan Card <dcard@umich.edu>
2022-01-07 18:40:56 -0700

    Updates

---
+++ precache-manifest.5464724786e152f1db911ede924e8a78.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b60cc10833f51db60238c32f87f51c7",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "2f1674cf18db68db8d52",
+    "url": "/website/static/css/main.0fceb42b.chunk.css"
+  },
+  {
+    "revision": "ffb70c40d92c59ec570c",
+    "url": "/website/static/js/2.a274831c.chunk.js"
+  },
+  {
+    "revision": "32eaebd0b06f4bb60d5e9e5d0e7cd13e",
+    "url": "/website/static/js/2.a274831c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1674cf18db68db8d52",
+    "url": "/website/static/js/main.e1f33ed7.chunk.js"
+  },
+  {
+    "revision": "1b8e03a275305b9f03bb",
+    "url": "/website/static/js/runtime-main.acca61c5.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/website/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "e6413c31180c114937622cbd9c4ed9c2",
+    "url": "/website/static/media/ExoTerra_Logo.e6413c31.webp"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/website/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/website/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/website/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/website/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/website/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/website/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "785010cafaca1a14f631c44bf6623a62",
+    "url": "/website/static/media/HET.785010ca.webp"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/website/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "76877c6acc416b7647d58f52b86a7ca2",
+    "url": "/website/static/media/NG_Logo.76877c6a.webp"
+  },
+  {
+    "revision": "93bfead8ffabe2a3948f27345fc3c788",
+    "url": "/website/static/media/PEPL_Logo.93bfead8.webp"
+  },
+  {
+    "revision": "7f5bdf606f78e1dd207531183319e4eb",
+    "url": "/website/static/media/UM_Logo.7f5bdf60.webp"
+  },
+  {
+    "revision": "f197063903c193c458c1a479dafe1aec",
+    "url": "/website/static/media/dancard.f1970639.webp"
+  },
+  {
+    "revision": "74f7ef916d4c778e50435198ad411e47",
+    "url": "/website/static/media/dj4e.74f7ef91.webp"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/website/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/website/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/website/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/website/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/website/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/website/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/website/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/website/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/website/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/website/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/website/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/website/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/website/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/website/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/website/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "3b90fd33889ad10e6aeac0064b532706",
+    "url": "/website/static/media/facility_effects.3b90fd33.webp"
+  },
+  {
+    "revision": "bc39c6dcbb4d66f809b17c35d8f360ab",
+    "url": "/website/static/media/hovercraft.bc39c6dc.webp"
+  },
+  {
+    "revision": "43da59243bff18e806d843f15c0f0485",
+    "url": "/website/static/media/lilac.43da5924.webp"
+  },
+  {
+    "revision": "75f269f62bd71ac5929aed50c0a08e0f",
+    "url": "/website/static/media/p4e.75f269f6.webp"
+  },
+  {
+    "revision": "a384e927fd0e4b99684636b430ca7443",
+    "url": "/website/static/media/python_ds.a384e927.webp"
+  },
+  {
+    "revision": "1312f9da54ed5400b70c15981de47d86",
+    "url": "/website/static/media/ramjet_diag.1312f9da.webp"
+  },
+  {
+    "revision": "7d7fb706fdd63d64ea3596d813fff3cf",
+    "url": "/website/static/media/ssme25.7d7fb706.webp"
+  },
+  {
+    "revision": "5dd1c6bc3e186c30c93c5a9e01ba9867",
+    "url": "/website/static/media/truss_deform.5dd1c6bc.gif"
+  },
+  {
+    "revision": "c5cfa839c3cabfbd35227fb40ec33062",
+    "url": "/website/static/media/voyager2.c5cfa839.webp"
+  }
+]);
