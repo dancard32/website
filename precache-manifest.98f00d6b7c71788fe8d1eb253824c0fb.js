ddb9d1208d74dfc94e1f69895a4659cb5a9b29cf
Dan Card <dcard@umich.edu>
2022-01-15 14:41:47 -0700

    Updates

---
+++ precache-manifest.98f00d6b7c71788fe8d1eb253824c0fb.js
@@ -0,0 +1,390 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b1a5c321ce45576049bb56a62bfbf0",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "038a6f4b3c0f165e7766",
+    "url": "/website/static/css/main.85ac3df7.chunk.css"
+  },
+  {
+    "revision": "e65cb62d1b44d9a8cebd",
+    "url": "/website/static/js/2.45a93818.chunk.js"
+  },
+  {
+    "revision": "32eaebd0b06f4bb60d5e9e5d0e7cd13e",
+    "url": "/website/static/js/2.45a93818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038a6f4b3c0f165e7766",
+    "url": "/website/static/js/main.f33a54fa.chunk.js"
+  },
+  {
+    "revision": "1b8e03a275305b9f03bb",
+    "url": "/website/static/js/runtime-main.acca61c5.js"
+  },
+  {
+    "revision": "c7f5a19b7452f4e9830ca46e388f9707",
+    "url": "/website/static/media/2kW HET.c7f5a19b.pdf"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/website/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "c3c10b67d0cdc55c4528f48dfef5e5e3",
+    "url": "/website/static/media/Applied Machine Learning in Python.c3c10b67.pdf"
+  },
+  {
+    "revision": "1f78fd1e4dc11c3ba9f3dabd7bbd8e4c",
+    "url": "/website/static/media/Applied Plotting, Charting & Data Representation in Python.1f78fd1e.pdf"
+  },
+  {
+    "revision": "917e7edb072446ffe42cd3bc0fe181b0",
+    "url": "/website/static/media/Applied Social Network Analysis in Python.917e7edb.pdf"
+  },
+  {
+    "revision": "28f65c2fcbfa76e7ca093586fbe7d337",
+    "url": "/website/static/media/Applied Text Mining in Python.28f65c2f.pdf"
+  },
+  {
+    "revision": "affe6b34b5517d7bef680c781482534b",
+    "url": "/website/static/media/Arduino-logo.affe6b34.webp"
+  },
+  {
+    "revision": "80d5121ddcb88820357b7ec487c493db",
+    "url": "/website/static/media/Building Web Applications in Django.80d5121d.pdf"
+  },
+  {
+    "revision": "2cd2f98b390e4077ea98b9fbf043a35e",
+    "url": "/website/static/media/C++-logo.2cd2f98b.webp"
+  },
+  {
+    "revision": "d516aa48f3c9ba91d3b163a6d7a96c18",
+    "url": "/website/static/media/CSS3-logo.d516aa48.webp"
+  },
+  {
+    "revision": "80f7bd1efdc3cf2d6d1f6e6e4600a5af",
+    "url": "/website/static/media/Cura-logo.80f7bd1e.webp"
+  },
+  {
+    "revision": "e6413c31180c114937622cbd9c4ed9c2",
+    "url": "/website/static/media/ExoTerra_Logo.e6413c31.webp"
+  },
+  {
+    "revision": "8da9ead5ae634a65f1338bb876a8d6db",
+    "url": "/website/static/media/ExpressPCB-logo.8da9ead5.webp"
+  },
+  {
+    "revision": "9dd114a44e69cefe4211ec8d25bf9250",
+    "url": "/website/static/media/Facility Effects.9dd114a4.pdf"
+  },
+  {
+    "revision": "92d3112068aa522ee836dce904e41f93",
+    "url": "/website/static/media/Fusion-logo.92d31120.webp"
+  },
+  {
+    "revision": "546ddfd5d903430abbe4f7539e798e40",
+    "url": "/website/static/media/Git-logo.546ddfd5.webp"
+  },
+  {
+    "revision": "61628e5d5afd2d89fe2c1f4f0878419d",
+    "url": "/website/static/media/Github-logo.61628e5d.webp"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/website/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/website/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/website/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/website/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/website/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/website/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "785010cafaca1a14f631c44bf6623a62",
+    "url": "/website/static/media/HET.785010ca.webp"
+  },
+  {
+    "revision": "58b986fe65304dd3e3ef8c384d0bc917",
+    "url": "/website/static/media/HTML5-logo.58b986fe.webp"
+  },
+  {
+    "revision": "0faee5a77d0ec513da50b21bfc5d68cd",
+    "url": "/website/static/media/Hovercraft Presentation.0faee5a7.pdf"
+  },
+  {
+    "revision": "fd51ef4f9a1cb70c74f341e46ef05c0c",
+    "url": "/website/static/media/Introduction to Data Science in Python.fd51ef4f.pdf"
+  },
+  {
+    "revision": "73acb315305de953379e4c096a12b7ff",
+    "url": "/website/static/media/LaTeX-logo.73acb315.webp"
+  },
+  {
+    "revision": "2696468af39afb60c7b988655fcf022d",
+    "url": "/website/static/media/Linux-logo.2696468a.webp"
+  },
+  {
+    "revision": "9910a094be30e45abf3338b800e7ed53",
+    "url": "/website/static/media/Lunar Lander Truss.9910a094.pdf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/website/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f31534daab8a0e6586c322a223632e68",
+    "url": "/website/static/media/Multimeter-logo.f31534da.webp"
+  },
+  {
+    "revision": "76877c6acc416b7647d58f52b86a7ca2",
+    "url": "/website/static/media/NG_Logo.76877c6a.webp"
+  },
+  {
+    "revision": "5d1c963db9d951daff64aee348ecf1ef",
+    "url": "/website/static/media/NX-logo.5d1c963d.webp"
+  },
+  {
+    "revision": "e97aadb9667c4e47c05655a3f9f57165",
+    "url": "/website/static/media/Oscope-logo.e97aadb9.webp"
+  },
+  {
+    "revision": "93bfead8ffabe2a3948f27345fc3c788",
+    "url": "/website/static/media/PEPL_Logo.93bfead8.webp"
+  },
+  {
+    "revision": "31198dbb3d3a2cfb786a4d5e0b4ad557",
+    "url": "/website/static/media/Programming for Everybody (Getting Started with Python).31198dbb.pdf"
+  },
+  {
+    "revision": "65cc2d8cd9b0abe014531704ce99e807",
+    "url": "/website/static/media/Python Data Structures.65cc2d8c.pdf"
+  },
+  {
+    "revision": "d9fab85ae3722810a45498f89ac38dd3",
+    "url": "/website/static/media/Python-logo.d9fab85a.webp"
+  },
+  {
+    "revision": "618a56310f183ab95efa022db62ed4f9",
+    "url": "/website/static/media/React-logo.618a5631.webp"
+  },
+  {
+    "revision": "2b1d1f8eeaeefad1ca75f80aacaa2b24",
+    "url": "/website/static/media/Regeneratively Cooled Nozzle Channels.2b1d1f8e.pdf"
+  },
+  {
+    "revision": "72e203ccab363326f67c7eb4a905d026",
+    "url": "/website/static/media/Resume.72e203cc.pdf"
+  },
+  {
+    "revision": "98ce9353fd0960f134e2340efa358633",
+    "url": "/website/static/media/SURE 2019 Proposal.98ce9353.pdf"
+  },
+  {
+    "revision": "fd9d974b3e00efd7b35f5d4b29a55f65",
+    "url": "/website/static/media/SWX-logo.fd9d974b.webp"
+  },
+  {
+    "revision": "496efd8010c885bcf6296879a963a465",
+    "url": "/website/static/media/Sqlite-logo.496efd80.webp"
+  },
+  {
+    "revision": "0431e3c2903f9030d98510fdbbf0ce46",
+    "url": "/website/static/media/Supersonic Engine Analysis.0431e3c2.pdf"
+  },
+  {
+    "revision": "7f5bdf606f78e1dd207531183319e4eb",
+    "url": "/website/static/media/UM_Logo.7f5bdf60.webp"
+  },
+  {
+    "revision": "28e8b06e823f04487a0429be8acdb6d0",
+    "url": "/website/static/media/Ubuntu-logo.28e8b06e.webp"
+  },
+  {
+    "revision": "7d49a231cff96722214f180d977c2dbc",
+    "url": "/website/static/media/Using Databases with Python.7d49a231.pdf"
+  },
+  {
+    "revision": "305b87bfebae87ac47c56fc57e5ffbc3",
+    "url": "/website/static/media/Using Python to Access Web Data.305b87bf.pdf"
+  },
+  {
+    "revision": "fad800f4413ba3543b47c6538a6cb205",
+    "url": "/website/static/media/Voyager 2.fad800f4.pdf"
+  },
+  {
+    "revision": "bdb7a84c26f6b73a9a1ed38913e1d12c",
+    "url": "/website/static/media/Web Application Technologies and Django.bdb7a84c.pdf"
+  },
+  {
+    "revision": "8c96ba69216b0dd050ac1c0b6b03a191",
+    "url": "/website/static/media/dan_card_transcript.8c96ba69.pdf"
+  },
+  {
+    "revision": "f197063903c193c458c1a479dafe1aec",
+    "url": "/website/static/media/dancard.f1970639.webp"
+  },
+  {
+    "revision": "74f7ef916d4c778e50435198ad411e47",
+    "url": "/website/static/media/dj4e.74f7ef91.webp"
+  },
+  {
+    "revision": "8687b5bccda5225b344cf470581112c3",
+    "url": "/website/static/media/django_webApps.8687b5bc.webp"
+  },
+  {
+    "revision": "c52094e0e9689ed31080d7bea22e13c7",
+    "url": "/website/static/media/django_webTech.c52094e0.webp"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/website/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/website/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/website/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/website/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/website/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/website/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/website/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/website/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/website/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/website/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/website/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/website/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/website/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/website/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/website/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "3b90fd33889ad10e6aeac0064b532706",
+    "url": "/website/static/media/facility_effects.3b90fd33.webp"
+  },
+  {
+    "revision": "bc39c6dcbb4d66f809b17c35d8f360ab",
+    "url": "/website/static/media/hovercraft.bc39c6dc.webp"
+  },
+  {
+    "revision": "43da59243bff18e806d843f15c0f0485",
+    "url": "/website/static/media/lilac.43da5924.webp"
+  },
+  {
+    "revision": "bb19f867f886ec7c61b5249d9d12ac19",
+    "url": "/website/static/media/mcsl.bb19f867.webp"
+  },
+  {
+    "revision": "75f269f62bd71ac5929aed50c0a08e0f",
+    "url": "/website/static/media/p4e.75f269f6.webp"
+  },
+  {
+    "revision": "aea7e706f22e02ca44670e1fe791adb3",
+    "url": "/website/static/media/p4e_dataStructures.aea7e706.webp"
+  },
+  {
+    "revision": "26c2dafd27cf7972e4d92d3c89cafb74",
+    "url": "/website/static/media/p4e_databases.26c2dafd.webp"
+  },
+  {
+    "revision": "75f269f62bd71ac5929aed50c0a08e0f",
+    "url": "/website/static/media/p4e_gettingStarted.75f269f6.webp"
+  },
+  {
+    "revision": "05cf6539af9ad0c83e837f60f34efc12",
+    "url": "/website/static/media/p4e_webData.05cf6539.webp"
+  },
+  {
+    "revision": "92aba1f68e630333e86ab00228f366a9",
+    "url": "/website/static/media/pds_dataRep.92aba1f6.webp"
+  },
+  {
+    "revision": "42fd009820c27ee18e8fb7e3424f05a2",
+    "url": "/website/static/media/pds_dataScience.42fd0098.webp"
+  },
+  {
+    "revision": "ebb8a7065723dcf012859795c0053b9b",
+    "url": "/website/static/media/pds_machineLearning.ebb8a706.webp"
+  },
+  {
+    "revision": "c9c44fa66031c66e967f8b0e0cc9abac",
+    "url": "/website/static/media/pds_networkAnalysis.c9c44fa6.webp"
+  },
+  {
+    "revision": "d477f757f1b4d1c594a3d6f6dd6d4561",
+    "url": "/website/static/media/pds_textMining.d477f757.webp"
+  },
+  {
+    "revision": "a384e927fd0e4b99684636b430ca7443",
+    "url": "/website/static/media/python_ds.a384e927.webp"
+  },
+  {
+    "revision": "1312f9da54ed5400b70c15981de47d86",
+    "url": "/website/static/media/ramjet_diag.1312f9da.webp"
+  },
+  {
+    "revision": "7d7fb706fdd63d64ea3596d813fff3cf",
+    "url": "/website/static/media/ssme25.7d7fb706.webp"
+  },
+  {
+    "revision": "5dd1c6bc3e186c30c93c5a9e01ba9867",
+    "url": "/website/static/media/truss_deform.5dd1c6bc.gif"
+  },
+  {
+    "revision": "c5cfa839c3cabfbd35227fb40ec33062",
+    "url": "/website/static/media/voyager2.c5cfa839.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d492fb4e29741f35efa58c1ee3d415c7.js"
+  "/website/precache-manifest.98f00d6b7c71788fe8d1eb253824c0fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
